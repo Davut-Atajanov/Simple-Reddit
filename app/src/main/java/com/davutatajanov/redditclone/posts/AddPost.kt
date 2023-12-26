80d534bf9544abad613fba34627dc9128f162d92
Davut Atajanov <commonappacc@gmail.com>
2023-12-26 07:35:52 +0300

    AddPost activity was created

---
+++ app/src/main/java/com/davutatajanov/redditclone/posts/AddPost.kt
@@ -0,0 +1,12 @@
+package com.davutatajanov.redditclone.posts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davutatajanov.redditclone.R
+
+class AddPost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+    }
+}
