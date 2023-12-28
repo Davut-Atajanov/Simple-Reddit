db91630c3d182df8fd72c981dc1f86d88eb647f3
Davut Atajanov <commonappacc@gmail.com>
2023-12-28 05:57:01 +0300

    home activity created and fully implemented

--- app/src/main/java/com/davutatajanov/redditclone/home/HomeActivity.kt
+++ app/src/main/java/com/davutatajanov/redditclone/home/HomeActivity.kt
@@ -2,11 +2,16 @@ package com.davutatajanov.redditclone.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.davutatajanov.redditclone.R
+import com.davutatajanov.redditclone.db.BlogPost
 import com.davutatajanov.redditclone.db.BlogPostViewModel
+import com.davutatajanov.redditclone.posts.FeedContent
 
 class HomeActivity : AppCompatActivity() {
 
@@ -20,13 +25,29 @@ class HomeActivity : AppCompatActivity() {
         listViewPersonalPosts = findViewById(R.id.listViewPersonalPosts)
         blogPostViewModel = ViewModelProvider(this).get(BlogPostViewModel::class.java)
 
-//        val data = blogPostViewModel.readAllData.observe(
-//
-//        )
-//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
-//        listViewTitle.adapter = adapter
 
+        blogPostViewModel.readAllData.observe(this, Observer { it ->
+            val uniqueContentValues = it.distinctBy { it.topic }
 
+            val titles = uniqueContentValues.map { it.topic }
 
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, titles)
+            val postAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, it)
+
+            // Set the adapter to the ListView
+            listViewTitle.adapter = adapter
+            listViewPersonalPosts.adapter = postAdapter
+            Log.d("Personal Posts",titles.toString())
+
+            listViewTitle.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                val selectedTitle = titles[position]
+                // Exclude posts with the selected title from it
+                val selectedPosts = it.filterNot { it.topic == selectedTitle }
+
+                val postAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, selectedPosts)
+                listViewPersonalPosts.adapter = postAdapter
+            }
+
+        })
     }
 }
